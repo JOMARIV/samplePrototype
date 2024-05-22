a8e9e8a6c98e0444f54178c9ccc2296cedd1d79c
dab-adu <danielle.angeli.blanco@adamson.edu.ph>
2024-05-22 21:08:48 +0800

    Add files via upload

---
+++ samplePrototype/managerMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace samplePrototype
+{
+    public partial class managerMenu : Form
+    {
+        public managerMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            orderHistory oh = new orderHistory();
+
+            oh.Show();
+
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            loginForm lg = new loginForm();
+
+            lg.Show();
+
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            employeeList el = new employeeList();
+
+            el.Show();
+
+            this.Close();
+        }
+    }
+}
